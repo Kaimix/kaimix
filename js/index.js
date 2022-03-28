9880e5a158b82dd13df5e86b2e615f33c8a2418f
Cristian Vidal <cristiangvidal99@gmail.com>
2022-03-28 11:44:15 -0300

    index.js

---
+++ js/index.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+            mensajeSuccess();
+            form.reset();
+        }
+      }).catch(error => {
+        mensajeError();
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
+
+// Funciones alertas
+
+function mensajeError() {
+    Swal.fire({
+        icon: "error",
+        title: "Falló el envio",
+        text: "Falló el envio, intente nuevamente"
+    });
+}
+
+function mensajeSuccess() {
+    Swal.fire({
+        icon: "success",
+        title: "Correo Enviado",
+        text: "El mensaje se envió correctamente"
+    });
+}
